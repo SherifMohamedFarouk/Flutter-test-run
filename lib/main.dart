e51e9f1d6ff6cd9d9e9d3f24a3da700492a64054
Shiro <sherifmohamed2004@gmail.com>
2020-09-21 19:01:34 +0200

    material App

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,17 @@ import "package:flutter/material.dart";
 void main(){
 
   runApp(
-      new MaterialApp(title: "My Flutter app", home:
-      new Material(
-          color: Colors.lightBlueAccent,
-          child: Center(
-              child :Text("hello flutter" , textDirection: TextDirection.ltr , style:TextStyle(color: Colors.white,fontSize: 40.0) )
-          )
-      )
+      new MaterialApp(title: "My Flutter app", home:Scaffold(
+        appBar: AppBar(title: Text("Flutter test"),
+        ),
+        body: Material(
+            color: Colors.lightBlueAccent,
+            child: Center(
+                child :Text("hello flutter" , textDirection: TextDirection.ltr , style:TextStyle(color: Colors.white,fontSize: 40.0) )
+            )
+        )
+      ),
+
       )
 
 
