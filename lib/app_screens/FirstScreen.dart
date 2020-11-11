c0d59dc0678eb4225bb11dfdeaca480481c477f9
Shiro <sherifmohamed2004@gmail.com>
2020-11-11 16:25:32 +0200

    Long list view

--- lib/app_screens/FirstScreen.dart
+++ lib/app_screens/FirstScreen.dart
@@ -2,40 +2,78 @@ import 'dart:math';
 
 import "package:flutter/material.dart";
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child:Container(
-      alignment: Alignment.center,
-      color: Colors.amberAccent,
-      padding: EdgeInsets.only(left: 10.0,top:40.0),
-      child:Column(
-        children:<Widget>[
-          Row(children: [
-            Expanded(child : Text(" jaban",textDirection: TextDirection.ltr,style: TextStyle(decoration:TextDecoration.none , fontSize: 35.0)))
-            , Expanded(child : Text(" the flight is gonna start soon please take your seat",textDirection: TextDirection.ltr,style: TextStyle(decoration:TextDecoration.none , fontSize: 20.0))
-            ),
-      ]
-    ),
-
-          Expanded(child : Text(" next flight is in an hour ",textDirection: TextDirection.ltr,style: TextStyle(decoration:TextDecoration.none , fontSize: 20.0))
-          ),
-         addingImage()
-        ],
-
-      )
-    )
-    );
+    return Center(
+        child: Container(
+            alignment: Alignment.center,
+            color: Colors.amberAccent,
+            padding: EdgeInsets.only(left: 10.0, top: 40.0),
+            child: Column(
+              children: <Widget>[
+                Row(children: [
+                  Expanded(
+                      child: Text(" jaban",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                              decoration: TextDecoration.none,
+                              fontSize: 35.0))),
+                  Expanded(
+                      child: Text(
+                          " the flight is gonna start soon please take your seat",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                              decoration: TextDecoration.none,
+                              fontSize: 20.0))),
+                ]),
+                Expanded(
+                    child: Text(" next flight is in an hour ",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none, fontSize: 20.0))),
+                addingImage(),
+                addingButton()
+              ],
+            )));
   }
-
 }
 
-class addingImage extends StatelessWidget{
+class addingImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     AssetImage assetImage = AssetImage('images/ic_launcher.png');
-    Image image =Image(image: assetImage);
-    return Container(child: image,width: 250.0,height: 250.0,);
+    Image image = Image(image: assetImage);
+    return Container(
+      child: image,
+      width: 250.0,
+      height: 250.0,
+    );
   }
+}
 
+class addingButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RaisedButton(
+        color: Colors.pink,
+        child: Text("press this button",
+            style: TextStyle(color: Colors.white, fontSize: 20.0)),
+        elevation: 6.0,
+        onPressed: () {
+          alertDialog(context);
+        },
+      ),
+    );
+  }
+}
+
+void alertDialog(BuildContext context) {
+  var alertDialog = AlertDialog(
+    title: Text("this is a dialog"),
+    content: Text("You have pressed this button "),
+  );
+
+  showDialog(context: context, builder: (BuildContext context) => alertDialog);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:math';
+
 
 import "package:flutter/material.dart";
 
@@ -7,9 +7,49 @@ import 'app_screens/FirstScreen.dart';
 
 
 void main (){
-  runApp(MaterialApp( debugShowCheckedModeBanner: false, title: "Flutter app", home: Home(),
+  runApp(MaterialApp( debugShowCheckedModeBanner: false, title: "Flutter app", home: Scaffold( body: getListView2()),
 
 
   ));
 }
 
+Widget getListView(){
+  var listView = ListView(
+    children: [
+      ListTile(
+        leading:Icon(Icons.landscape),
+        title: Text("this is a view "),
+        subtitle: Text("nubmer one "),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: (){
+          debugPrint("hey hey");
+        },
+
+      )
+    ],
+  );
+  return listView;
+
+}
+
+List<String> getlistString(){
+  var items = List<String>.generate(1000, (index) => "Item $index");
+  return items;
+
+}
+
+Widget getListView2(){
+  var listItem = getlistString();
+  var listView = ListView.builder(itemBuilder: (context ,index){
+
+       return ListTile(
+         leading: Icon(Icons.access_time),
+         title: Text(listItem[index]),
+         onTap: (){
+           debugPrint("this is a list view ");
+         },
+       );
+  }
+  );
+  return listView;
+}
