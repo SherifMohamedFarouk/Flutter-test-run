20f40b3253fc6c177f25db3da682153c50373251
Shiro <sherifmohamed2004@gmail.com>
2021-02-15 17:35:54 +0200

    ListView and Snack bar and FLoating action button

--- lib/app_screens/Home.dart
+++ lib/app_screens/Home.dart
@@ -23,8 +23,53 @@ class Home extends StatelessWidget {
                   child: Text("rooowwsss"),
                 ),
               ],
-            ),Text("helloo this a column ")
+            ),
+            Text("helloo this a column "),
+            Expanded(child: AssetImag()),
+            Expanded(child: Button()),
           ],
         ));
   }
+}
+
+class AssetImag extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('images/ic_launcher.png');
+    Image image = Image(image: assetImage);
+    return Container(
+      child: image,
+      width: 250.0,
+      height: 250.0,
+    );
+  }
+}
+
+class Button extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 250.0,
+      height: 250.0,
+      child: RaisedButton(
+        color: Colors.amber,
+        child: Text(
+          "click me to book a flight",
+          style: TextStyle(fontSize: 40.0, color: Colors.white),
+        ),
+        onPressed: () {BookFLight(context);},
+      ),
+    );
+  }
+}
+
+void BookFLight(BuildContext covariant){
+  AlertDialog alertDialog = AlertDialog(title:Text( "you have booked your flight"),
+    content: Text("have a pleasent flight"),
+  );
+   showDialog(context: covariant,
+     builder: (BuildContext covariant){
+     return alertDialog;
+     }
+   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,63 @@ class MyFlutterApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: Text("my first app")),
         body: Center(
-          child: Home(),
-        ),
+          child: getList(),
+
+        ),floatingActionButton: FloatingActionButton(
+         onPressed: () => debugPrint("hey"),
+        child: Icon(Icons.add),
+        tooltip: 'Add another item',
+      ),
       ),
     );
   }
 }
+  void showSnackBar(BuildContext context , String item){
+   var snackBar = SnackBar(
+     content: Text("You have clicked this item $item"),
+     action: SnackBarAction(label: "undo",
+       onPressed: (){
+       debugPrint('print');
+       },
+     ),
+   );
+   Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  List<String> getListView(){
+  var items = List<String>.generate(100, (index) => "items $index");
+  return items;
+  }
+
+Widget getList() {
+  var listview = ListView.builder(
+      itemBuilder:(context , index){
+         return ListTile(
+           title: Text(getListView()[index]),
+           onTap:() => showSnackBar(context,getListView()[index]),
+         );
+      }
+  );
+  return listview;
+}
+
+
+// Widget getList() {
+//   var listview = ListView(children: [
+//     ListTile(
+// leading: Icon(Icons.ac_unit),
+//       title: Text("Ac unit"),
+//       subtitle: Text(" 12 hourse"),
+//       trailing:Icon(Icons.wb_sunny),
+//       onTap:() => debugPrint("u clicked"),
+//     ),
+//     ListTile( leading: Icon(Icons.ac_unit),
+//       title: Text("Ac unit"),
+//
+//     ),
+//     Container(color: Colors.amber,child: Text("hey hey"),)
+//
+//   ],
+//   );
+//   return listview;
+// }
